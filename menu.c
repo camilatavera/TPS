37e0026d1ae00549ef371b5e186e9513edf28b49
camilatavera <camilatavera01@gmail.com>
2021-06-24 21:35:32 -0300

    CON LOS ARCHIVOS DE PROYECTO

---
+++ menu.c
@@ -0,0 +1,19 @@
+#include "menu.h"
+#include <stdio.h>
+
+void printMenu(void){
+
+	printf("***************************   MENU     *************************** \n"
+
+			"1. Cargar los datos de los empleados desde el archivo data.csv (modo texto). \n"
+			"2. Cargar los datos de los empleados desde el archivo data.bin (modo binario) \n"
+			"3. Alta de empleado \n"
+			"4. Modificar datos de empleado \n"
+			"5. Baja de empleado\n"
+			"6. Listar empleados\n"
+			"7. Ordenar empleados\n"
+			"8. Guardar los datos de los empleados en el archivo data.csv (modo texto).\n"
+			"9. Guardar los datos de los empleados en el archivo data.bin (modo binario).\n"
+			"10. Salir  \n"
+			"******************************************************************* \n");
+}
---
+++ parser.h
@@ -0,0 +1,23 @@
+/*
+ * parser.h
+ *
+ *  Created on: Jun 5, 2021
+ *      Author: caami
+ */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+
+
+int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
+
+int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
+
+
+
+#endif /* PARSER_H_ */
